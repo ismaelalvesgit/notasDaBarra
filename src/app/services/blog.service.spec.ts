f1318261729672564c1c60ab99ae14130a9a1cef
ismaelalvesbit <cearaismael1997@gmail.com>
2019-11-07 15:06:23 -0300

    test in services

--- src/app/services/blog.service.spec.ts
+++ src/app/services/blog.service.spec.ts
@@ -1,28 +1,36 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { BlogService } from './blog.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Blog } from '../models/blog.model';
 
 describe('test - BlogService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports:[
-      HttpClientTestingModule
-    ],
-    providers: [
-      BlogService,
-    ],
-  }));
+  let injector: TestBed;
+  let service: BlogService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports:[
+        HttpClientTestingModule
+      ],
+      providers: [
+        BlogService,
+      ],
+    });
+    injector = getTestBed();
+    service = injector.get(BlogService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
 
   it('instaciamento do metodo', () => {
-    const service: BlogService = TestBed.get(BlogService);
     expect(service).toBeTruthy();
   });
 
   it('get / blog', ()=>{
-    const service: BlogService = TestBed.get(BlogService);
     service.getBlog().subscribe((rs)=>{
-      expect(rs).toBe(4)
-      expect(rs).toContain(Blog)
-    })
-  })
+      expect(rs).toBe(4);
+      expect(rs).toContain(Blog);
+    });
+  });
+  
 });
